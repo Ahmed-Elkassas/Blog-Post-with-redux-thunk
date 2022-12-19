c54d018befa5e87b372733077c1db9ae2ea9e7ee
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-12-19 15:54:25 +0200

    Create SinglePost page and install react-router-dom

---
+++ src/components/posts/SinglePost.js
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { useSelector } from 'react-redux'
+
+import PostAuthor from "../users/PostAuthor";
+import TimeAgo from "../TimeAgo";
+import { selectPostById } from '../../redux/reducers/posts-slice';
+
+
+export const SinglePost = () => {
+    const { postId } = useParams()
+
+    const post = useSelector((state) => selectPostById(state, Number(postId)))
+
+    if (!post) {
+        return (
+            <section>
+                <h2>Post not found!</h2>
+            </section>
+        )
+    }
+
+    return (
+        <article>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p className="postDetails">
+                <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+        </article>
+    )
+}
+
--- src/redux/reducers/posts-slice.js
+++ src/redux/reducers/posts-slice.js
@@ -58,6 +58,9 @@ export const getAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 
+export const selectPostById = (state, postId) =>
+    state.posts.posts.find(post => post.id === postId);
+
 export const { addNewPost } = postSlice.actions;
 
 export const postReducer = postSlice.reducer;
