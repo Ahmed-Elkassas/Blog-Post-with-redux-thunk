6c9ec915d6f3554e5ec4c2c80a1681f466ce8007
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-12-20 11:58:17 +0200

    Add routes to App component wrapped with Layout component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import { PostForm } from "./components/posts/PostForm";
 import { PostsList } from "./components/posts/PostsList";
+import { SinglePost } from "./components/posts/SinglePost";
 
 
 function App() {
   return (
-    <main className="App">
-      <PostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route index element={<PostsList />} />
+      <Route  path='post' >
+        <Route index element={<PostForm />} />
+        <Route path=":id" element={<SinglePost />} />
+      </Route>
+    </Routes>
   );
 }
 
